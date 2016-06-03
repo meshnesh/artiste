30af7303cc2bfb1e9d08a7c77fe170c30b786020
meshnesh <tonny.nesh@gmail.com>
2016-06-04 01:43:50 +0300

    added time loader script

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
 $(document).foundation()
+$(function () {
+
+    // Returns width of browser viewport
+    var browser = $(window).width();
+    // Returns width of HTML document
+    var document = $(document).width();
+
+    $('.nav .toggle-nav').click(function (e) {
+        $('.nav .nav-mobile').addClass('style-mobile').slideToggle('slow');
+        e.preventDefault();
+    });
+
+});
+
+
+setTimeout(function (i) {
+    showElements($('#parent'));
+}, 5000);
+
+function showElements(element) {
+    element.children('div').each(function () {
+                if ($(this).is(':hidden')) {
+                    $(this).fadeIn();
+                    setTimeout(function () {
+                        showElements(element)
+                    }, 5000);
+                    return false;
+                }
